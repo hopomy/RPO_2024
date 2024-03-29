b92950820cc9da4bb1d3d705d90369532846ce31
nikitakalmackiy <neketkalm@gmail.com>
2024-03-29 13:18:04 +0300

    laba4 done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 dependencies {
     implementation("commons-codec:commons-codec:1.15")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ("org.apache.commons:commons-io:1.3.2")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("commons-io:commons-io:2.6")
