f4e5e3d518a74ffc49c523d08a516631e408f47c
nikitakalmackiy <neketkalm@gmail.com>
2024-03-29 11:13:21 +0300

    laba2 done

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,9 +1,20 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import java.util.Arrays;
 
@@ -15,42 +26,68 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("fclient");
         System.loadLibrary("mbedcrypto");
-        System.out.println("Done!!!!!!!!");
     }
 
     private ActivityMainBinding binding;
+    ActivityResultLauncher activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         int res = initRng();
-        byte[] v = randomBytes(10);
+        byte[] key = randomBytes(16);
+        byte[] data = randomBytes(32);
 
-        byte[] key = new byte[16];
-        byte[] data = new byte[32];
         byte[] encrypted = encrypt(key, data);
-        byte[] decrypted = decrypt(key, data);
+        System.out.println(Arrays.toString(data));
         System.out.println(Arrays.toString(encrypted));
-        System.out.println(Arrays.toString(decrypted));
+        System.out.println(Arrays.toString(decrypt(key, encrypted)));
+
         System.out.println(res);
-        System.out.println(Arrays.toString(v));
+        System.out.println(Arrays.toString(key));
 
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data1 = result.getData();
+                        // обработка результата
+                        String pin = data1.getStringExtra("pin");
+                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
-
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        activityResultLauncher.launch(it);
     }
 
     /**
      * A native method that is implemented by the 'fclient' native library,
      * which is packaged with this application.
      */
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
-
 }
--- app/src/main/java/ru/iu3/fclient/PinpadActivity.java
+++ app/src/main/java/ru/iu3/fclient/PinpadActivity.java
@@ -1,16 +1,58 @@
 package ru.iu3.fclient;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class PinpadActivity extends Activity {
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PinpadActivity extends AppCompatActivity {
+
     TextView tvPin;
     String pin = "";
+    final int MAX_KEYS = 10;
+
+    protected void ShuffleKeys()
+    {
+        Button keys[] = new Button[] {
+                findViewById(R.id.btnKey0),
+                findViewById(R.id.btnKey1),
+                findViewById(R.id.btnKey2),
+                findViewById(R.id.btnKey3),
+                findViewById(R.id.btnKey4),
+                findViewById(R.id.btnKey5),
+                findViewById(R.id.btnKey6),
+                findViewById(R.id.btnKey7),
+                findViewById(R.id.btnKey8),
+                findViewById(R.id.btnKey9),
+        };
+
+        byte[] rnd = MainActivity.randomBytes(MAX_KEYS);
+        for(int i = 0; i < MAX_KEYS; i++)
+        {
+            int idx = (rnd[i] & 0xFF) % 10;
+            CharSequence txt = keys[idx].getText();
+            keys[idx].setText(keys[i].getText());
+            keys[i].setText(txt);
+        }
+    }
+
+    public void keyClick(View v)
+    {
+        String key = ((TextView)v).getText().toString();
+        int sz = pin.length();
+        if (sz < 4)
+        {
+            pin += key;
+            tvPin.setText("****".substring(3 - sz));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,81 +67,12 @@ public class PinpadActivity extends Activity {
             Intent it = new Intent();
             it.putExtra("pin", pin);
             setResult(RESULT_OK, it);
-            Toast.makeText(this, pin, Toast.LENGTH_SHORT).show();
             finish();
+        });
 
+        findViewById(R.id.btnReset).setOnClickListener((View) -> {
+            pin = "";
+            tvPin.setText("");
         });
-        findViewById(R.id.btnKey1).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey2).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey3).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey4).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey5).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey6).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey7).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey8).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey9).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-        findViewById(R.id.btnKey0).setOnClickListener((View v) -> {
-            keyClick(v);
-        });
-    }
-    protected void keyClick(View v){
-        String key = ((TextView)v).getText().toString();
-        int sz = pin.length();
-        if (sz < 4){
-            pin += key;
-            tvPin.setText("****".substring(3 - sz));
-        }
-    }
-
-    final int MAX_KEYS = 10;
-
-    protected void ShuffleKeys() {
-        Button keys[] = new Button[]{
-                findViewById(R.id.btnKey0),
-                findViewById(R.id.btnKey1),
-                findViewById(R.id.btnKey2),
-                findViewById(R.id.btnKey3),
-                findViewById(R.id.btnKey4),
-                findViewById(R.id.btnKey5),
-                findViewById(R.id.btnKey6),
-                findViewById(R.id.btnKey7),
-                findViewById(R.id.btnKey8),
-                findViewById(R.id.btnKey9),
-        };
-
-        byte[] rnd = MainActivity.randomBytes(MAX_KEYS);
-        for (int i = 0; i < MAX_KEYS; i++) {
-            int idx = (rnd[i] & 0xFF) % 10;
-            CharSequence txt = keys[idx].getText();
-            keys[idx].setText(keys[i].getText());
-            keys[i].setText(txt);
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK || data != null) {
-                String pin = data.getStringExtra("pin");
-                Toast.makeText(this, pin, Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
