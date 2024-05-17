765e52560de3db6c1b8ad875ddb5b82a750437e8
nikitakalmackiy <neketkalm@gmail.com>
2024-05-17 21:48:07 +0300

    laba11 done

--- backend/front/src/App.js
+++ backend/front/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {useState} from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,24 +7,45 @@ import NavigationBar from "./components/NavigationBarClass";
 import Home from "./components/Home";
 import Another_Home from "./components/Another_Home";
 import Login from "./components/Login";
+import Utils from "./utils/Utils";
+import SideBar from "./components/SideBar";
+import CountryListComponent from "./components/CountryListComponent";
+import CountryComponent from "./components/CountryComponent";
 
-function App() {
-  return (
-      <div className="App">
-        {/* Смысл в том, что браузер привязывает компоненты к локальным путям внутри приложения */}
-        <BrowserRouter>
-          <NavigationBar/>
-          <div className="container-fluid">
-            <Routes>
-              <Route path="home" element={<Home />} />
-              {/* Дополнение: создал новый компонент, чтобы разделить то, что выводится с каждой из ссылок */}
-              <Route path="Another_Home" element={<Another_Home />} />
-              <Route path="login" element={<Login/>} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-  );
+import {connect} from "react-redux";
+
+const ProtectedRoute = ({children}) => {
+    let user = Utils.getUser();
+    return user ? children : <Navigate to={'/login'}/>
+};
+
+const App = props => {
+
+const [exp,setExpanded] = useState(true);
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavigationBar toggleSideBar={() =>
+                    setExpanded(!exp)}/>
+                    <div className="wrapper">
+                        <SideBar expanded={exp} />
+                        <div className="container-fluid">
+                            { props.error_message &&  <div className="alert alert-danger m-1">{props.error_message}</div>}
+                            <Routes>
+                                <Route path="login" element={<Login />}/>
+                                <Route path="home" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+                                <Route path="countries" element={<ProtectedRoute><CountryListComponent/></ProtectedRoute>}/>
+                                <Route path="countries/:id" element={<ProtectedRoute><CountryComponent/></ProtectedRoute>}/>
+                            </Routes>
+                        </div>
+                    </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App;
+const mapStateToProps = function (state) {
+    const {msg} = state.alert;
+    return {error_message: msg};
+}
+export default connect(mapStateToProps)(App);
